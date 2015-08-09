b6bb5f808bd6442b08261439bbc3bcbcfe0b617b
alexkonovalov <alvkonovalov@gmail.com>
2015-08-09 16:19:47 +0300

    first commit

---
+++ Content/App/src/ak-slider.js
@@ -0,0 +1,29 @@
+﻿(function() {
+    "use strict";
+    var mod = angular.module('angular-slider', []);
+    mod.directive('akSlider',
+        function() {
+            return {
+                restrict: "A",
+                scope: {
+                    onSubmit: "&",
+                    slide: "="
+                },
+                link: function(scope, element, attrs) {
+
+                    function getSlidesDom() {
+                        return element[0].querySelectorAll('ul[ak-slider] > li');
+                    }
+
+                    scope.$watch(function() {
+                        angular.forEach(getSlidesDom(), function(child, index) {
+                            child.style.cssText = 'transform :translate3d(' + (index - scope.slide) * 100 + '%,0px, 0px); -webkit-transform : translate3d(' + (index - scope.slide) * 100 + '%,0px, 0px); position : absolute;';
+
+                        });
+                    });
+
+                }
+            };
+        }
+    );
+})();
