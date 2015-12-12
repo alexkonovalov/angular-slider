5fc512c380d29dadf6cc12960f191f0e18dda396
alexkonovalov <alvkonovalov@gmail.com>
2015-12-13 00:20:23 +0300

    fixed demo upload dependency issue

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,6 +16,18 @@ module.exports = function(grunt) {
                 cwd : '<%= globalConfig.source %>',
                 src: ['*'],
                 dest : '<%= globalConfig.output %>/'
+            },
+            demo_lib : {
+                expand : true,
+                cwd : 'dist',
+                src: ['ak-slider.min.js', 'ak-slider.min.css'],
+                dest : '<%= globalConfig.demo %>/lib/slider'
+            },
+            demo_bower: {
+                expand : true,
+                cwd : 'bower_components',
+                src: ['**'],
+                dest : '<%= globalConfig.demo %>/lib/bower_components'
             }
         },
         "gh-pages": {
@@ -25,13 +37,14 @@ module.exports = function(grunt) {
             src: ['**']
         },
         clean: {
-            build: ["<%= globalConfig.output %>/"]
+            build: ["<%= globalConfig.output %>/"],
+            demo: ["<%= globalConfig.demo %>/lib/"]
         },
         uglify: {
             options: {
                 compress: true,
                 mangle: true,
-                sourceMap: true
+                sourceMap: false
             },
             jstarget: {
                 src: "<%= globalConfig.output %>/<%= globalConfig.buildFileName %>.js",
@@ -58,7 +71,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks("grunt-contrib-cssmin");
     grunt.loadNpmTasks('grunt-contrib-clean');
 
-    grunt.registerTask("default", [ "clean","copy", "uglify", "cssmin"]);
-    grunt.registerTask("publish", ["default", "gh-pages"])
+    grunt.registerTask("default", [ "clean:build", "copy:dist_files", "uglify", "cssmin"]);
+    grunt.registerTask("publish", ["default", "clean:demo", "copy:demo_lib", "copy:demo_bower", "gh-pages"])
 
 };
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # angular-slider
 Angular slideshow directive
+#[Demo](http://alexkonovalov.github.io/angular-slider/)
