284d6562b6008970a96b8152e4d7fd68fc7dce6c
alexkonovalov <alvkonovalov@gmail.com>
2015-12-14 00:08:30 +0300

    removed color slides.changed styling

--- demo/scripts/demoController.js
+++ demo/scripts/demoController.js
@@ -8,52 +8,16 @@
     // Define the controller on the module.
     // Inject the dependencies. 
     // Point to the controller definition function.
-    function demoController($scope) {
+    function demoController($scope, randomDataService) {
 
-        $scope.slide = 0;
-        $scope.items = [
-             { color: "#00BB00", text: "slide 1" },
-             { color: "#BBBB00", text: "slide 2" },
-             { color: "#00BBBB", text: "slide 3" },
-             { color: "#0000BB", text: "slide 4" }
-        ];
-
-        $scope.next = function () {
-            if($scope.slide >= $scope.items.length - 1)
-            {
-                return;
-            }
-
-            $scope.slide++;
-        };
-
-        $scope.prev = function () {
-            if($scope.slide <= 0)
-            {
-                return;
-            }
-
-            $scope.slide--;
-        };
-
-        $scope.unshiftItemsAndStay = function () {
-            $scope.items.unshift({ color: "#B0BB6B"});
-            $scope.items.unshift({ color: "#BB00BB" });
-            $scope.slide = $scope.slide + 2;
-
-        }
-
-        $scope.pushItemsAndStay = function () {
-            $scope.items.push({ color: "#B0BB6B", text: "extra slide 1" });
-            $scope.items.push({ color: "#BB00BB", text: "extra slide 2" });
-        }
 
 
         $scope.imgSlide = 0;
+
         $scope.imgItems = [
-            { source: "images/img_0.jpg", alt: "planet_0" },
-            { source: "images/img_1.jpg", alt: "planet_1" },
-            { source: "images/img_2.jpg", alt: "planet_2" }
+            getRandomImg(),
+            getRandomImg(),
+            getRandomImg()
         ];
 
         $scope.imgNext = function () {
@@ -74,8 +38,28 @@
             $scope.imgSlide--;
         };
 
+        function getRandomImg () {
+            var imgIndex = randomDataService.getRandomInt(0,4);
+
+            return {
+                source: "images/img_" + imgIndex + ".jpg",
+                alt: "planet_" + imgIndex
+            }
+        }
+
+        $scope.unshiftItemsAndStay = function () {
+            $scope.imgItems.unshift(getRandomImg());
+            $scope.imgItems.unshift(getRandomImg());
+            $scope.imgSlide += 2;
+
+        }
+
+        $scope.pushItemsAndStay = function () {
+            $scope.imgItems.push(getRandomImg());
+            $scope.imgItems.push(getRandomImg());
+        }
 
     }
 
-    angular.module('sildeDemoApp').controller(controllerId, ['$scope', demoController]);
+    angular.module('sildeDemoApp').controller(controllerId, ['$scope', 'randomDataService', demoController]);
 })();
---
+++ demo/scripts/randomDataService.js
@@ -0,0 +1,17 @@
+﻿
+
+(function () {
+    'use strict';
+
+    var serviceId = 'randomDataService';
+
+    function randomDataService($scope) {
+
+        this.getRandomInt = function(min, max){
+            return Math.floor(Math.random() * (max - min + 1)) + min;
+        };
+
+    }
+
+    angular.module('sildeDemoApp').service(serviceId, [randomDataService]);
+})();
